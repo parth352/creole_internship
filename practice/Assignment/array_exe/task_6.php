c0c763363dcfcb401b1ffd43b0f79939d8b59afe
parth patel <patelparthjayantilal93@gmail.com>
2023-02-01 18:54:09 +0530

    Practice assignment in php

--- practice/Assignment/array_exe/task_6.php
+++ practice/Assignment/array_exe/task_6.php
@@ -19,33 +19,4 @@
         echo "[$i] => $arr[$i],";
     }
     echo ")";
-
-
-     //$new_arr=[];
-   // $val=2;
-   // $update_arr=[];
-    // // for($index=0; $index<=2; $index++)
-    // // {
-    // //     echo "[$i]=>";
-    // //     print_r($arr[$i]);
-    // // }
-    // for($index=1; $index<=3; $index++)
-    // {
-    //     array_push($new_arr,$arr[$index]);
-    // }
-    //  //print_r($new_arr);
-
-    // $update_arr=$new_arr;
-    
-    // foreach($update_arr as $final_arr=> $final_arr_value)
-    // {
-    //     // $update_arr[$key]= $val;
-    //     // $val++;
-    
-    //     // $final_arr = 1;
-        
-    //     echo "$final_arr_value,";
-    //     // print_r($final_arr_value);
-    // }
-   
 ?>
--- practice/Assignment/for_each_loop_exe/task_2.php
+++ practice/Assignment/for_each_loop_exe/task_2.php
@@ -1,6 +1,5 @@
 <?php
     $personal_details = array("name"=>"Rajesh Rao", "occupation" => "Engineer",
     "age" => 39, "country" => "India");
-    
     print_r($personal_details);
 ?>
