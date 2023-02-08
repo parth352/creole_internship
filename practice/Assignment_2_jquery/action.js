b1d2ac1a31547d0f134aaa440a3f618e0d59de08
parth patel <patelparthjayantilal93@gmail.com>
2023-02-08 23:23:12 +0530

    Practice for jQuery

--- practice/Assignment_2_jquery/action.js
+++ practice/Assignment_2_jquery/action.js
@@ -2,13 +2,24 @@ $(document).ready(function(){
 
     //1. Blinking effect
     $('#blinking').on('click',function(){
-        $('p.blinking').fadeOut();
+        $('p.blinking').fadeToggle();
     });
 
     //2. Limit no of characters in the input field.
-    $('#text').on('keydown',function(){
-        $('#remaining_text').text("hello");
-    });
+         var temp = '';
+        $('#input').on("keypress", function() {
+          var count = $('#input').val().length;
+          if (count > 10) {
+          	temp += $('#input').val().substring(9)
+              $('#output').text(temp);
+              $('#input').val( $('#input').val().substring(0, 9));
+          }
+        //   else if(count < 10)
+        //   {
+        //     $('#output').hide();
+        //   }
+        });
+      
 
     //3. This is to underline text
     $('#under_line').on('click',function(){
@@ -22,22 +33,23 @@ $(document).ready(function(){
 
     //5. Change button text using jQuery
     $('#change_text').on('click',function(){
-        $('p.change_text').text("This is my new text");
+        $('#change_text').text("This is my new text");
     });
 
     //6. Add options to a drop-down list using jQuery.
     $('#drop_down').on('click',function(){
-        $('select').dropdown(2000);
+        $('#add_option')
+        .append("<option> New list </option>");
     });
 
     //7. Delete all table rows except first one using jQuery
     $('#display_first_row').on('click',function(){
-        $('tr:first');
+        $("table tr:not(:first-child)").remove();
     });
 
     //8. Remiove a link applied to any element using jQuery.
     $('#remove_link').on('click',function(){
-        $('a.remove_link').remove();
+        $('a').removeAttr("href");
     });
 
     // 9. Change background color of all the elements using jQuery
@@ -49,13 +61,13 @@ $(document).ready(function(){
 
     // 10. Using jQuery add the class " w3r_font_color " to the last paragraph element.
     $('#add_to_last_paragraph').on('click',function(){
-        $('p.add_to_last_paragraph').addClass("w3r_font_color");
+        $('p:last').addClass("w3r_color");
     });
 
     //11. Using jQuery insert some HTML after all paragraphs.
     $("#insert").click(function () {
-        $("p.insert").text(function (i, origtext) {
-            return origtext + " This is new inserted text (index: " + i + ")";
+        $("p.insert").text(function (i,origtext) {
+            return origtext + " This is new inserted text";
         });
     });
 
@@ -63,29 +75,19 @@ $(document).ready(function(){
      var count=0;
     $('#count_checkbox').on('click',function(){
         $('p.count').text(function(){
-
-                if($('input.checkbox1').val('car'))
-                {
-                    count++;
-                }
-
-                if($('input.checkbox2').val('truck'))
-                {
-                    count++;
-                }
-
-                if($('input.checkbox3').val('bike'))
-                {
-                    count++;
-                }
-
-            return count;
+            var checkbox = $('input[type="checkbox"]').length;
+            return checkbox;
         });
     });
 
+
     //13. Finds the checked radio input.
-
-
+    $('#radio_check').on('click',function(){
+        $('p.count_radio').text(function(){
+            var radio = $('input[type="radio"]').length;
+            return radio;
+        });
+    });
 
 
     //14.Border around all list items that are children of a specified class of an unordered list.
@@ -95,11 +97,15 @@ $(document).ready(function(){
           .css({ color: "red", border: "2px solid red" }); // filter method will return the element of class name filter_method. Always assign .filter_method to specify class
       });
 
-      
+
     // 15. Change the color of any paragraph to red on mouseover event.
     $('p.mouse_over').on('mouseover',function(){
         $(this).css({"color":"red"});
     });
 
+    //16. Finds every paragraph element on the page and add red border to all paragraph.
+    $("#change_paragraph_p").on("click", function () {
+        $("div.change_paragraph_p").children("p").css({ border: "2px solid red" }); 
+      });
 
 });
